1a8cb3b405614e58a23248c3e44465baf3871a43
Madi Rysbekov <hooizit@gmail.com>
2019-08-20 02:47:45 +0600

    refactor: ownProps

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -12,19 +12,7 @@ import "./book-list.css";
 
 class BookList extends Component {
   componentDidMount() {
-    const {
-      bookstoreService,
-      booksLoaded,
-      booksRequested,
-      booksError
-    } = this.props;
-    booksRequested();
-    bookstoreService
-      .getBooks()
-      .then(data => {
-        booksLoaded(data);
-      })
-      .catch(err => booksError(err));
+    this.props.fetchBooks();
   }
   render() {
     const { books, loading, error } = this.props;
@@ -52,10 +40,18 @@ const mapStateToProps = ({ books, loading, error }) => {
   return { books, loading, error };
 };
 
-const mapDispatchToProps = {
-  booksLoaded,
-  booksRequested,
-  booksError
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    fetchBooks: () => {
+      dispatch(booksRequested());
+      ownProps.bookstoreService
+        .getBooks()
+        .then(data => {
+          dispatch(booksLoaded(data));
+        })
+        .catch(err => dispatch(booksError(err)));
+    }
+  };
 };
 
 export default compose(
