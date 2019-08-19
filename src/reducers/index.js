c5dd62a33b48f41c315cc3f8966e131241ca2532
Madi Rysbekov <hooizit@gmail.com>
2019-08-20 03:31:37 +0600

    new component to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,43 @@
 const initialState = {
   books: [],
   loading: true,
-  error: null
+  error: null,
+  cartItems: [
+    {
+      id: 1,
+      name: "book 1",
+      count: 3,
+      total: 90
+    },
+    {
+      id: 2,
+      name: "book 2",
+      count: 2,
+      total: 100
+    }
+  ],
+  orderTotal: 190
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
       return {
+        ...state,
         books: [],
         loading: true,
         error: null
       };
     case "FETCH_BOOKS_SUCCESS":
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null
       };
     case "FETCH_BOOKS_FAILURE":
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload
