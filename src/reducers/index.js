2c230b9a5a0344bef859723b1d53cc19e71a10bc
Madi Rysbekov <hooizit@gmail.com>
2019-08-19 20:44:24 +0600

    store data read

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,16 @@
 const initialState = {
-  books: []
+  books: [
+    {
+      id: 1,
+      title: "Prodaction-Ready Microsevices",
+      author: "Susan J. Fowler"
+    },
+    {
+      id: 2,
+      title: "Release It!",
+      author: "Michael T. Nygard"
+    }
+  ]
 };
 
 const reducer = (state = initialState, action) => {
