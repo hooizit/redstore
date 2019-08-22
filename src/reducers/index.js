bd96aa31bc24e61624ba66cc93fc53d958253e95
Madi Rysbekov <hooizit@gmail.com>
2019-08-22 21:40:08 +0600

    add to cart: count item

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,24 @@ const initialState = {
   orderTotal: 190
 };
 
+const updateCartItems = (cartItems, item, idx) => {
+  if (idx === -1) {
+    return [...cartItems, item];
+  }
+  return [...cartItems.slice(0, idx), item, ...cartItems.slice(idx + 1)];
+};
+
+const updateCartItem = (book, item = {}) => {
+  const { id = book.id, title = book.title, count = 0, total = 0 } = item;
+
+  return {
+    id,
+    title,
+    count: count + 1,
+    total: total + book.price
+  };
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
@@ -32,16 +50,15 @@ const reducer = (state = initialState, action) => {
     case "BOOK_ADDED_TO_CART":
       const bookId = action.payload;
       const book = state.books.find(book => book.id === bookId);
-      const newItem = {
-        id: book.id,
-        name: book.title,
-        count: 1,
-        total: book.price
-      };
+      const itemIdx = state.cartItems.findIndex(book => book.id === bookId);
+      const item = state.cartItems[itemIdx];
+
+      const newItem = updateCartItem(book, item);
       return {
         ...state,
-        cartItems: [...state.cartItems, newItem]
+        cartItems: updateCartItems(state.cartItems, newItem, itemIdx)
       };
+
     default:
       return state;
   }
